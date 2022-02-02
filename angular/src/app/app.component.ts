dc9f68bd3a6ba8191d6c73b2233a25e261dfcc2e
Anuroop <anuroopjoy@gmail.com>
2022-02-02 13:39:13 +0530

    update

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  SwUpdate,
+  UnrecoverableStateEvent,
+  VersionEvent,
+  VersionReadyEvent,
+} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private updateService: SwUpdate) {}
+  ngOnInit() {
+    console.log('AppComponent.ngOnInit');
+    this.updateService.versionUpdates.subscribe((event: VersionEvent) => {
+      console.log(event);
+      alert(event.type);
+      if (
+        event.type === 'VERSION_READY' &&
+        confirm(
+          `New version ${
+            (event as VersionReadyEvent).latestVersion.hash
+          } available. Load New Version?`
+        )
+      ) {
+        window.location.reload();
+      }
+    });
+    // const interval = setInterval(async () => {
+    //   const shouldUpdate = await this.updateService.checkForUpdate();
+    //   alert('Checked for update with result: ' + shouldUpdate);
+    //   if (shouldUpdate) {
+    //     const result = await this.updateService.activateUpdate();
+    //     alert('Activate Update completed with result: ' + result);
+    //     clearInterval(interval);
+    //   }
+    // }, 1000);
+
+    this.updateService.unrecoverable.subscribe(
+      (event: UnrecoverableStateEvent) => {
+        alert('Error reason : ' + event.reason);
+      }
+    );
+  }
 }
